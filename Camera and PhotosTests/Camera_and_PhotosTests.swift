70d2a7c829fa132693f17b1e4314d792b6bf7b4a
Katie Wang <katie.wang1950f@gmail.com>
2020-06-09 10:33:31 -0400

    Initial Commit

---
+++ Camera and PhotosTests/Camera_and_PhotosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Camera_and_PhotosTests.swift
+//  Camera and PhotosTests
+//
+//  Created by Quan Wang on 6/9/20.
+//  Copyright © 2020 Katie Wang. All rights reserved.
+//
+
+import XCTest
+@testable import Camera_and_Photos
+
+class Camera_and_PhotosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
